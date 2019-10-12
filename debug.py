3fe7f20fe74f88dfaf3ff558bd3d6adc6be6d555
liaogguoxiong <li>
2019-10-12 15:16:08 +0800

    无关紧要的改变

--- debug.py
+++ debug.py
@@ -9,13 +9,28 @@
 
 import os
 
-f_path="C:/Users/lgx/Downloads"
-file_list=os.listdir(f_path)
-exist=0
-for name in file_list:
-    if "9144030097935050X1F" in name:
-        print("在")
-        exist=1
+# f_path="C:/Users/lgx/Downloads"
+# file_list=os.listdir(f_path)
+# exist=0
+# for name in file_list:
+#     if "9144030097935050X1F" in name:
+#         print("在")
+#         exist=1
+#
+# if exist !=1:
+#     print("不在")
 
-if exist !=1:
-    print("不在")
+with open("input_file","r") as f:
+    r=f.readlines()
+    print(r)
+
+r1=[]
+print(len(r))
+for i in r:
+    if i != "\n":
+        i=i.strip()
+        r1.append(i)
+
+
+print(r1)
+print(len(r1))
